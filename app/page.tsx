89c0b3998ef35a9205c6d0f8964fedebfee675e3
Oludowole Olumide <xeonncodes@gmail.com>
2025-12-29 13:48:34 +0100

    Jhjbhj

--- app/page.tsx
+++ app/page.tsx
@@ -11,14 +11,14 @@ export default function Home() {
   return (
     <section className="flex flex-col items-center justify-center gap-4 py-8 md:py-10">
       <div className="inline-block max-w-xl text-center justify-center">
-        <span className={title()}>Make&nbsp;</span>
-        <span className={title({ color: "violet" })}>beautiful&nbsp;</span>
+        <span className={title()}>Create&nbsp;</span>
+        <span className={title({ color: "violet" })}>nice&nbsp;</span>
         <br />
         <span className={title()}>
-          websites regardless of your design experience.
+          websites regardless of your design architecture.
         </span>
         <div className={subtitle({ class: "mt-4" })}>
-          Beautiful, fast and modern React UI library.
+          Beautiful, fast and modern Next UI framework.
         </div>
       </div>
 
